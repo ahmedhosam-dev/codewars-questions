27e5dd11a6417d3c145795837bbf23149cdce998
ahmedhosam-dev <ahmed0114702@gmail.com>
2025-01-12 20:26:35 +0200

    Solve question 47

---
+++ Q47.Grasshopper-Messi-goals-function.cpp
@@ -0,0 +1,16 @@
+/** Description:
+ * Messi goals function
+ * Messi is a soccer player with goals in three leagues:
+ * - LaLiga
+ * - Copa del Rey
+ * - Champions
+ * Complete the function to return his total number of goals in all three leagues.
+ * Note: the input will always be valid.
+ * For example:
+ * 5, 10, 2  -->  17
+ * URL: https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+*/
+
+int goals (int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
